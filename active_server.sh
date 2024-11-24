fedfbf614d8d992f8b9d8084a8a8631f94c2da12
Kirill Myamishev <kirillmyam@yandex.ru>
2024-11-24 20:26:05 +0300

    startup/shutdown script add (add sleep)

--- active_server.sh
+++ active_server.sh
@@ -4,5 +4,6 @@ echo "Startup the server..."
 ufw allow 8000
 ufw allow 8001
 source /server/messenger_server/my-venv/bin/activate
+sleep 1
 python3 manage.py runserver 176.124.204.174:8000 &
 echo "Server startup completed!"
