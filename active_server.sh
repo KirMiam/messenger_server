535ce6206a4299fab9f577a9c03ed46aea6772cc
Kirill Myamishev <kirillmyam@yandex.ru>
2024-11-24 20:35:17 +0300

    startup/shutdown script add (my-env)

--- active_server.sh
+++ active_server.sh
@@ -3,7 +3,6 @@
 echo "Startup the server..."
 ufw allow 8000
 ufw allow 8001
-. /server/messenger_server/my-venv/bin/activate
-sleep 1
-python3 manage.py runserver 176.124.204.174:8000 &
+chmod 777 /server/messenger_server/my-venv/bin/python3
+/server/messenger_server/my-venv/bin/python3 manage.py runserver 176.124.204.174:8000 &
 echo "Server startup completed!"
--- disable_server.sh
+++ disable_server.sh
@@ -4,5 +4,4 @@ echo "Shutdown the server"
 ufw deny 8000
 ufw deny 8001
 pkill -f "python3 manage.py runserver 176.124.204.174:8000"
-deactivate
 echo "Server shutdown completed!"
