fd13b4dee858244ace77e6de93ce2d4a54ac8d81
Kirill Myamishev <kirillmyam@yandex.ru>
2024-11-26 21:14:55 +0300

    add last_login v4

--- authenticated.py
+++ authenticated.py
@@ -7,7 +7,7 @@ class IsAuthenticatedWithAddLastLogging(IsAuthenticated):
     def has_permission(self, request, view):
         if request.user and request.user.is_authenticated:
             user = request.user
-            user.last_login = time.time()
+            user.last_login = str(time.time())
             print(user.last_login)
             user.save(update_fields=['last_login'])
         return request.user and request.user.is_authenticated
