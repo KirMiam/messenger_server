29f8aafbb5ca2431ac551ec5966a5d3adbf224d3
Kirill Myamishev <kirillmyam@yandex.ru>
2024-12-03 14:03:39 +0300

    test v1

--- active_server.sh
+++ active_server.sh
@@ -4,5 +4,5 @@ echo "Startup the server..."
 ufw allow 8000
 ufw allow 8001
 chmod 777 /server/messenger_server/my-venv/bin/python3
-/server/messenger_server/my-venv/bin/python3 manage.py runserver 176.124.204.174:8000 &
+/server/messenger_server/venv/bin/python3 manage.py runserver 176.124.204.174:8000 &
 echo "Server startup completed!"
